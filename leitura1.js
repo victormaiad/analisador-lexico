e5caa91e93fccfb284e631569854768d016775e4
victor <victor.maia.dias2002@gmail.com>
2023-02-15 13:52:26 -0300

    +- certo

--- leitura1.js
+++ leitura1.js
@@ -1,147 +1,36 @@
-const fs = require('fs');
+function scanner(programa) {
+  // separa o programa em tokens (unidades léxicas)
+  var tokens = programa.match(/while|do|[ij]|[0-9]+|[<+=;]/g);
 
-// Tabela de palavras reservadas
-const reservedWords = {
-  while: 'Palavra reservada',
-  do: 'Palavra reservada',
-};
+  // itera sobre cada token e classifica seu tipo
+  for (var i = 0; i < tokens.length; i++) {
+    var token = tokens[i];
 
-// Tabela de símbolos
-const symbolTable = {};
-
-let line = 0;
-let column = 0;
-
-// Função para verificar se um caracter é um dígito
-function isDigit(char) {
-  return /\d/.test(char);
-}
-
-// Função para verificar se um caracter é um identificador
-function isIdentifier(char) {
-  return /[a-zA-Z]/.test(char);
-}
-
-// Função para verificar se um caracter é um operador
-function isOperator(char) {
-  return /[<+=]/.test(char);
-}
-
-// Função para verificar se um caracter é um terminador
-function isTerminator(char) {
-  return char === ';';
-}
-
-// Função para adicionar um símbolo na tabela de símbolos, caso ele não exista
-function addSymbol(symbol) {
-  if (!symbolTable[symbol]) {
-    const index = Object.keys(symbolTable).length + 1;
-    symbolTable[symbol] = index;
+    // verifica se é uma palavra reservada
+    if (token === "while" || token === "do") {
+      console.log("Palavra reservada: " + token);
+    }
+    // verifica se é um operador
+    else if (token === "<" || token === "=" || token === "+") {
+      console.log("Operador: " + token);
+    }
+    // verifica se é um terminador
+    else if (token === ";") {
+      console.log("Terminador: " + token);
+    }
+    // verifica se é um identificador
+    else if (token === "i" || token === "j") {
+      console.log("Identificador: " + token);
+    }
+    // verifica se é uma constante
+    else if (/^[0-9]+$/.test(token)) {
+      console.log("Constante: " + token);
+    }
+    // se não for nenhum dos tipos acima, é um token inválido
+    else {
+      console.log("Token inválido: " + token);
+    }
   }
 }
 
-// Função para analisar o arquivo e gerar as tabelas
-function analyzeFile(fileName) {
-  const data = fs.readFileSync(fileName, 'utf8');
-  let lexeme = '';
-
-  for (let i = 0; i < data.length; i++) {
-    const char = data[i];
-
-    if (char === '\n') {
-      line++;
-      column = 0;
-      continue;
-    }
-
-    if (char === ' ') {
-      column++;
-      continue;
-    }
-
-    // Identificação de palavras reservadas
-    if (char === 'w') {
-      lexeme += char;
-      i++;
-      column++;
-
-      if (data[i] === 'h' && data[i + 1] === 'i' && data[i + 2] === 'l' && data[i + 3] === 'e' && !isIdentifier(data[i + 4])) {
-        console.log(`${lexeme} ${reservedWords.while} ${lexeme.length} (${line}, ${column - lexeme.length})`);
-        lexeme = '';
-        continue;
-      } else {
-        i -= 3;
-        column -= 3;
-        lexeme = lexeme.slice(0, -1);
-      }
-    }
-
-    if (char === 'd') {
-      lexeme += char;
-      i++;
-      column++;
-
-      if (data[i] === 'o' && !isIdentifier(data[i + 1])) {
-        console.log(`${lexeme} ${reservedWords.do} ${lexeme.length} (${line}, ${column - lexeme.length})`);
-        lexeme = '';
-        continue;
-      } else {
-        i--;
-        column--;
-        lexeme = lexeme.slice(0, -1);
-      }
-    }
-
-    // Identificação de constantes e identificadores
-    if (isDigit(char) || isIdentifier(char)) {
-      lexeme += char;
-      column++;
-
-      while (isDigit(data[i + 1]) || isIdentifier(data[i + 1])) {
-        lexeme += data[i + 1];
-        i++;
-        column++;
-      }
-
-      if (isIdentifier(lexeme[0])) {
-        addSymbol(lexeme);
-        console.log(`${lexeme} [Identificador (${symbolTable[lexeme]})] ${lexeme.length} (${line}, ${column - lex}`)
-      }
-
-      if (isDigit(lexeme[0])) {
-        console.log(`${lexeme} [Constante] ${lexeme.length} (${line}, ${column - lexeme.length})`);
-      }
-    
-      lexeme = '';
-      continue;
-    }
-    
-    // Identificação de operadores
-    if (isOperator(char)) {
-      console.log(`${char} [Operador] 1 (${line}, ${column})`);
-      column++;
-      continue;
-    }
-    
-    // Identificação de terminadores
-    if (isTerminator(char)) {
-      console.log(`${char} [Terminador] 1 (${line}, ${column})`);
-      column++;
-      continue;
-    }
-    
-    // Caracter inválido
-    console.log(`${char} [Erro] 1 (${line}, ${column})`);
-    column++;}
-
-    console.log('\nTabela de símbolos');
-    console.log('Índice\tSímbolo');
-    for (const symbol in symbolTable) {
-    console.log(${symbolTable[symbol]}\t${symbol});
-    }
-    }
-
-    // Chamada da função para análise do arquivo
-    analyzeFile('entrada.txt');
-    
-    
+scanner("while i < 100 do i = i + j;");
