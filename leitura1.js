9ff75a0b4c1cbb9a3d8bd8b93d796901bdf5dbbc
victor <victor.maia.dias2002@gmail.com>
2023-02-28 10:32:41 -0300

    last

--- leitura1.js
+++ leitura1.js
@@ -1,78 +1,88 @@
-const PALAVRA_CHAVE = ['while', 'do'];
-const OPERADOR = ['<', '=', '+'];
-const TERMINADOR = ';';
-const IDENTIFICADOR = ['i', 'j'];
+const input = 'whil i < 100 do i = i + j;';
 
-function lex(code) {
+const keywords = ['while', 'do'];
+const operators = ['<', '=', '+'];
+const terminator = ';';
+const identifiers = ['i', 'j'];
+
+let line = 0;
+let col = 0;
+
+const tokenize = (input) => {
   const tokens = [];
-  let pos = 0;
-  let line = 0;
-  let col = 0;
-  
-  while (pos < code.length) {
-    let char = code[pos];
-    
-    if (/\s/.test(char)) {
-      pos++;
+
+  let cursor = 0;
+
+  while (cursor < input.length) {
+    let currentChar = input[cursor];
+
+    if (currentChar === ' ') {
+      cursor++;
       col++;
+      continue;
     }
-    else if (char === TERMINADOR) {
-      tokens.push({ token: char, id: 'Terminador', tamanho: 1, line, col });
-      pos++;
+
+    if (currentChar === terminator) {
+      tokens.push({ token: terminator, identification: 'terminador', size: 1, position: `(${line},${col})` });
+      cursor++;
       col++;
+      continue;
     }
-    else if (/\d/.test(char)) {
-      let tamanho = 1;
-      let value = '';
-      while (/\d/.test(code[pos + tamanho])) {
-        value += code[pos + tamanho];
-        tamanho++;
-      }
-      tokens.push({ token: char + value, id: 'Constante', tamanho, line, col });
-      pos += tamanho;
-      col += tamanho;
-    }
-    else if (/[a-zA-Z]/.test(char)) {
-      let tamanho = 1;
-      let value = '';
-      while (/[a-zA-Z]/.test(code[pos + tamanho])) {
-        value += code[pos + tamanho];
-        tamanho++;
-      }
-      if (PALAVRA_CHAVE.includes(char + value)) {
-        tokens.push({ token: char + value, id: 'Palavra-Chave', tamanho, line, col });
-      }
-      else if (IDENTIFICADOR.includes(char)) {
-        tokens.push({ token: char, id: 'Identificador', tamanho: 1, line, col });
-      }
-      else {
-        throw new Error(`Invalid identifier '${char + value}' at line ${line}, column ${col}`);
-      }
-      pos += tamanho;
-      col += tamanho;
-    }
-    else if (OPERADOR.includes(char)) {
-      tokens.push({ token: char, id: 'Operador', tamanho: 1, line, col });
-      pos++;
+
+    if (operators.includes(currentChar)) {
+      tokens.push({ token: currentChar, identification: 'operador', size: 1, position: `(${line},${col})` });
+      cursor++;
       col++;
+      continue;
     }
-    else {
-      throw new Error(`Invalid character '${char}' at line ${line}, column ${col}`);
+
+    if (currentChar.match(/[0-9]/)) {
+      let sequence = currentChar;
+
+      while (cursor + 1 < input.length && input[cursor + 1].match(/[0-9]/)) {
+        sequence += input[cursor + 1];
+        cursor++;
+        col++;
+      }
+
+      if (sequence.length > 1) {
+        tokens.push({ token: sequence, identification: 'constante', size: sequence.length, position: `(${line},${col - sequence.length + 1})` });
+      } else {
+        tokens.push({ token: sequence, identification: 'number', size: sequence.length, position: `(${line},${col - sequence.length + 1})` });
+      }
+
+      cursor++;
+      col++;
+      continue;
     }
-    
-    if (char === '\n') {
-      line++;
-      col = 1;
+
+    if (currentChar.match(/[a-zA-Z]/)) {
+      let identifier = '';
+
+      while (input[cursor] && input[cursor].match(/[a-zA-Z]/)) {
+        identifier += input[cursor];
+        cursor++;
+        col++;
+      }
+
+      if (keywords.includes(identifier)) {
+        tokens.push({ token: identifier, identification: 'palavra reservada', size: identifier.length, position: `(${line},${col - identifier.length})` });
+      } else if (identifiers.includes(identifier)) {
+        tokens.push({ token: identifier, identification: 'identificador', size: identifier.length, position: `(${line},${col - identifier.length})` });
+      } else {
+        throw new Error(`Erro léxico: identificador '${identifier}' inválido em (${line},${col - identifier.length})`);
+      }
+
+      continue;
     }
+
+    throw new Error(`Erro léxico: caractere '${currentChar}' inválido em (${line},${col})`);
   }
-  
+
   return tokens;
-}
+};
 
+const tokens = tokenize(input);
 
-const code  = 'while i < 100 do i = i + j;';
-
-const tokens = lex(code);
-
+// Imprime a tabela
 console.table(tokens);
-
